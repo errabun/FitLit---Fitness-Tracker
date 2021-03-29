2c0dacfd23f5e3d21e8ad03c21255e98a0c2a81f
Caroline Eubanks <cmeubanks@gmail.com>
2021-03-29 15:19:15 -0600

    completed user repository class structure

---
+++ src/UserRepository.js
@@ -0,0 +1,20 @@
+const userData = require('./data/users');
+
+class UserRepository {
+  constructor(userData) {
+    this.userData = userData;
+  }
+
+  returnUserData(id) {
+    return this.userData.find(element => id === element.id)
+  }
+
+  calculateTotalAverageStepGoal() {
+    let avgStepGoal = this.userData.reduce((accu, cv) => {
+      return Math.round((accu + cv.dailyStepGoal) / this.userData.length)
+    }, 0);
+    return avgStepGoal;
+  }
+}
+
+module.exports = UserRepository;
