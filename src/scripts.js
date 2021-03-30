d463e31859f18eedd2ff1e5fee84fe51d4cd6360
Caroline Eubanks <cmeubanks@gmail.com>
2021-03-30 17:39:25 -0600

    fixed module exports

--- src/scripts.js
+++ src/scripts.js
@@ -1,11 +1,12 @@
 const userInfo = new UserRepository(userData)
 
 const greeting = document.querySelector('#greeting');
+const friends = document.querySelectorAll()
 
 window.addEventListener('load', generateFirstUser);
 
 function generateFirstUser() {
   const firstUser = new User(userInfo.returnUserData(3));
-  greeting.innerText = firstUser.returnUserFirstName(3);
+  greeting.innerText = `Welcome, ${firstUser.returnUserFirstName(3)}`;
 
 };
