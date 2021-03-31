c0b559caadd9f05200fae1667de9776e6080e0a0
Eric Rabun <errabun@gmail.com>
2021-03-31 15:59:15 -0600

    Add headshots in docs folder

---
+++ src/Hydration.js
@@ -0,0 +1,19 @@
+class Hydration {
+  constructor () {}
+
+  totalAvgFluidPerDay(id) {
+
+  }
+
+  fluidOzsDrankDay(id, date) {
+
+  }
+
+  fluidOzsDrankWeek(id, date) {
+    // return the amount for each day
+  }
+}
+
+if (typeof module !== 'undefined') {
+  module.exports = UserRepository;
+}
