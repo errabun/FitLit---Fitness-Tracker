133134a58fabd89c7341175bdd6b27207ccbe93f
Eric Rabun <errabun@gmail.com>
2021-03-31 17:38:01 -0600

    Refactor Hydration class methods; begin Hydration-test

--- src/Hydration.js
+++ src/Hydration.js
@@ -4,36 +4,6 @@ class Hydration {
     this.date = hydrationData.date;
     this.numOunces = hydrationData.numOunces;
   }
-
-  totalAvgFluidPerDay(id) {
-    let ozsPerDay = hydrationData.filter(user => {
-      return user.userID === this.id;
-    });
-    let totalAvg = ozsPerDay.reduce((total, user) => {
-      total += user.numOunces;
-      return total;
-    }, 0) / ozsPerDay.length;
-    return totalAvg;
-  }
-
-  fluidOzsDrankDay(id, date) {
-    let findUser = hydrationData.filter(user => {
-      return user.userID === this.id;
-    });
-    let ozsDayDrank = findUser.find(user => {
-      return user.date === this.date
-    });
-    return ozsDayDrank.numOunces;
-  }
-
-  fluidOzsDrankWeek(id, dateStart, dateEnd) {
-    let findUserData = hydrationData.filter(user => {
-      return user.userID === this.id;
-    });
-    return findUserData.filter(user => {
-      return user.date >= dateStart && user.date <= dateEnd;
-    })
-  }
 }
 
 if (typeof module !== 'undefined') {
--- src/HydrationRepository.js
+++ src/HydrationRepository.js
@@ -4,10 +4,34 @@ class HydrationRepository {
   }
 
   returnUserHydration(id) {
-    return this.hydrationData.filter(element => id === element.id);
+    return this.hydrationData.filter(user =>  user.userID === id);
   };
+
+  totalAvgFluidPerDay(id) {
+    let ozsPerDay = this.returnUserHydration(id);
+    let totalAvg = ozsPerDay.reduce((total, user) => {
+      return total + user.numOunces;
+    }, 0)
+    return Math.round(totalAvg / ozsPerDay.length);
+  };
+
+  fluidOzsDrankDay(id, date) {
+    let findUser = this.returnUserHydration(id);
+    let ozsDayDrank = findUser.find(user => {
+      return user.date === this.date
+    });
+    return ozsDayDrank.numOunces;
+  };
+
+  fluidOzsDrankWeek(id, dateStart, dateEnd) {
+    let findUserData = this.returnUserHydration(id);
+    let weekOzDrank = findUserData.filter(user => {
+      return user.date >= dateStart && user.date <= dateEnd;
+    })
+    return weekOzDrank; 
+  }
 }
 
 if (typeof module !== 'undefined') {
-  module.exports = UserHydration;
+  module.exports = HydrationRepository;
 }
