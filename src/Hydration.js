eeeaae059f64779f7415ac9e49cf4861e875eb19
Eric Rabun <errabun@gmail.com>
2021-03-31 16:00:38 -0600

    Create totalAvgFluidDay method

--- src/Hydration.js
+++ src/Hydration.js
@@ -2,18 +2,20 @@ class Hydration {
   constructor () {}
 
   totalAvgFluidPerDay(id) {
-
+    let ozsPerDay = hydrationData.filter(user => {
+      return user.userID === id;
+    })
+    let totalAvg = ozsPerDay.reduce((total, user) => {
+      total += user.numOunces;
+      return total;
+    }, 0) / ozsPerDay.length;
+    return totalAvg;
   }
 
-  fluidOzsDrankDay(id, date) {
 
-  }
 
-  fluidOzsDrankWeek(id, date) {
-    // return the amount for each day
-  }
 }
 
 if (typeof module !== 'undefined') {
-  module.exports = UserRepository;
+  module.exports = Hydration;
 }
