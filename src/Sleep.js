2b177026b968b2ed0e0d07d80e792cca6a9d2f7c
Caroline Eubanks <cmeubanks@gmail.com>
2021-04-01 08:59:47 -0600

    added 3 more sleep methods

--- src/Sleep.js
+++ src/Sleep.js
@@ -12,4 +12,28 @@ class Sleep {
     })
     return totalHours
   }
+
+  showHoursSleptByDate(date) {
+     const sleepOverDate = this.userSleepData.filter(element => date === element.date);
+     const hours = sleepOverDate.map(element => element.hoursSlept);
+     const [sleptHours] = hours;
+     return sleptHours;
+   }
+
+   showSleepQualityByDate(date) {
+     const qualityOverDate = this.userSleepData.filter(element => date === element.date);
+     const num = qualityOverDate.map(element => element.sleepQuality);
+     const [sleepQuality] = num;
+     return sleepQuality;
+   }
+
+   showHoursSleptByWeek(id, date) {
+    const userSleepDates = this.userSleepData.map(user => user.date);
+    const getDateIndex = userSleepDates.indexOf(date);
+    const sleepUserDate = this.userSleepData.slice(getDateIndex, getDateIndex + 7);
+    return this.userSleepData.reduce((obj, sleep) => {
+      obj[sleep.date] = sleep.hoursSlept;
+      return obj;
+    }, {})
+  }
 }
