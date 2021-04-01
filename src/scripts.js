7fa071a9b83e109311973bfbf239101ba8620fb3
Caroline Eubanks <cmeubanks@gmail.com>
2021-04-01 13:43:34 -0600

    completed DOM manipulation of step data for user

--- src/scripts.js
+++ src/scripts.js
@@ -4,6 +4,8 @@ let firstUser = new User(userInfo.returnUserData(3));
 
 const greeting = document.querySelector('#greeting');
 const friends = document.querySelectorAll('.friend');
+const userStep = document.querySelector('#userStepGoal');
+const averageStep = document.querySelector('#averageStepGoal');
 // console.log(friends);
 
 window.addEventListener('load', generateFirstUser);
@@ -12,6 +14,7 @@ function generateFirstUser() {
   // const firstUser = new User(userInfo.returnUserData(3));
   greeting.innerText = `Welcome, ${firstUser.returnUserFirstName(3)}`;
   displayFriends();
+  displayStepGoal();
 };
 
 function displayFriends() {
@@ -26,3 +29,8 @@ function displayFriends() {
   friends[2].innerText = arrayOfFriends[2];
   friends[3].innerText = arrayOfFriends[3];
 };
+
+function displayStepGoal() {
+  userStep.innerText = `Your Daily Step Goal: ${firstUser.dailyStepGoal}`;
+  averageStep.innerText = `Average Step Goal for all Users: ${userInfo.calculateTotalAverageStepGoal()}`;
+}
