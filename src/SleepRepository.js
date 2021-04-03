c5f01392e965eed1e259e3cfc837a9f7e9b5eb2d
Caroline Eubanks <cmeubanks@gmail.com>
2021-04-03 09:50:12 -0600

    added average sleep quality for all users functionality

--- src/SleepRepository.js
+++ src/SleepRepository.js
@@ -6,4 +6,10 @@ class SleepRepository {
   returnUserSleepData(id) {
     return this.sleepData.filter(element => id === element.userID);
   }
+
+  returnAverageSleepQuality() {
+    const sleepQual = this.sleepData.map(data => data.sleepQuality)
+    const averageQual = sleepQual.reduce((sum, sleepNum) => sum + sleepNum)
+    return averageQual/sleepQual.length;
+  }
 }
